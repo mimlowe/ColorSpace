3b894c264f5dfa7801e89d0f40eeeeef58711a09
Wayne Arnold <wayne.arnold@sjsu.edu>
2019-11-10 22:25:09 -0800

    Fix references not showing directly in output, add util for importing colors

--- flask-backend/backend.py
+++ flask-backend/backend.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Flask
 from flask_mongoengine import MongoEngine
 from flask_mongorest import MongoRest
@@ -7,14 +9,24 @@ from flask_mongorest import operators as ops
 from flask_mongorest import methods
 from mongoengine.fields import *
 from mongoengine.fields import BooleanField, StringField, ListField, IntField
+from getpass import getpass
 
 app = Flask(__name__)
 
+# Prompts for password instead of entering
+mongo_pass = getpass('Enter mongodb password:\n')
+
 app.config.update(
-    MONGODB_HOST = 'localhost',
+    MONGODB_HOST = 'mongodb://cluster0-shard-00-00-aaxvq.mongodb.net:27017,cluster0-shard-00-01-aaxvq.mongodb.net:27017,cluster0-shard-00-02-aaxvq.mongodb.net:27017/?ssl=true&authSource=admin&retryWrites=true&w=majority',
     MONGODB_PORT = 27017,
-    MONGODB_DB = 'mongorest_example_app',
+    MONGODB_DB = 'mongorest_test',
+    MONGODB_USERNAME = 'wayne',
+    MONGODB_PASSWORD = mongo_pass
 )
+
+# mongodb+srv://<username>:<password>@cluster0-aaxvq.mongodb.net/test?retryWrites=true&w=majority
+# mongodb://<username>:<password>@cluster0-shard-00-00-aaxvq.mongodb.net:27017,cluster0-shard-00-01-aaxvq.mongodb.net:27017,cluster0-shard-00-02-aaxvq.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority
+
 
 db = MongoEngine(app)
 api = MongoRest(app)
@@ -35,34 +47,41 @@ class SiteGetterResource(Resource):
 class Color(db.Document):
     hexval = db.StringField(max_length=8, required=True) #0x000000
     rgb = db.ListField(db.StringField(max_length=3), required=True)
-    color_group = db.ReferenceField(ColorGroup)
+    colorgroup = db.ReferenceField(ColorGroup)
     # pass
+
 
 class Site(db.Document):
     domain = db.StringField(unique=True, required=True)
     sitegetter = db.EmbeddedDocumentField(SiteGetter)
     colors = db.ListField(db.ReferenceField(Color)) # TODO: Does this need to be db.Ref?
+    color = db.ReferenceField(Color)
 
 class ColorGroupResource(Resource):
     document = ColorGroup
-
-class SiteResource(Resource):
-    document = Site
-    related_resources = {
-        'sitegetter':SiteGetterResource,
-    }
-    filters = {
-        'name': [ops.Exact, ops.Startswith]
-    }
 
 class ColorResource(Resource):
     document = Color
     filters = {
         'hexval': [ops.Exact, ops.Startswith]
     }
-    rename_fields = {
-        'hexval':'hex'
+    # rename_fields = {
+    #     'hexval':'hex'
+    # }
+
+
+class SiteResource(Resource):
+    document = Site
+    related_resources = {
+        'sitegetter':SiteGetterResource,
+        'colors':ColorResource
     }
+    filters = {
+        'domain': [ops.Exact, ops.Startswith],
+        # Needs to be queried with the document ID
+        'colors': [ops.Exact]
+    }
+
 
 @api.register(name='sites', url='/sites/')
 class SiteView(ResourceView):
@@ -83,5 +102,57 @@ class ColorGroupView(ResourceView):
 curl -H "Content-Type: application/json" -X POST -d '{"domain":"aa.test.com","colors":["5dc8436d4d04f2d62c3cd837","5dc8415800b62bc7cad59b7f"], "sitegetter":{"src":"style.css","options":[""]}}' http://127.0.0.1:5000/sites/
 '''
 
+def init_data():
+    dark = ColorGroup(primary='dark',grayscale=True)
+    dark.save()
+    print('dark ID {}'.format(str(dark.id)))
+    light = ColorGroup(primary='light',grayscale=True)
+    light.save()
+    print('light ID {}'.format(str(light.id)))
+    black = Color(hexval='000000',rgb=['0','0','0'],colorgroup=dark)
+    black.save()
+    print('black ID {}'.format(str(black.id)))
+    white = Color(hexval='FFFFFF',rgb=['255','255','255'],colorgroup=light)
+    white.save()
+    print('white ID {}'.format(str(white.id)))
+    example = Site(domain='www.example.com', sitegetter={'src':'style.css','options':['']}, colors=[white,black])
+    example.save()
+    print('example site ID {}'.format(str(example.id)))
+
+    return dark, light, black, white, example
+
+def delete_data(documents):
+
+    for item in documents:
+        item.delete()
+
+def color_import():
+
+    with open('colors.json') as f:
+        colors = json.loads(f.read())
+    
+    for color_in in colors:
+        pass
+        # Search for color group
+        # If color group DNE, create with name
+        # Include logic to assess whether the color is light, dark, greyscale
+        # Create color and (optionally) assign to color group
+        
+        # 
+        r = color_in['rgb']['r']
+        g = color_in['rgb']['g']
+        b = color_in['rgb']['b']
+        rgb = [r,g,b]
+
+        # if int(r) > 128 and int(g) > 128 and int(b) > 128:
+        #      color_group
+        # color_group = ColorGroup()
+        color = Color(hexval=color_in['hexstring'[1:7]],
+                      rgb=rgb,
+                      )
+        color.save()
+
 if __name__ == "__main__":
+
     app.run(debug=True)
+    pass
